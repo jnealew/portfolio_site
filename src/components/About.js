3b07b5da090b37dba39ddd25a76d3535af73f8ee
Jack Neale <jack@192-168-1-113.tpgi.com.au>
2022-12-01 10:38:23 +1100

    Updated about sections (test)

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,9 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
+                My name is Jack Neale, I am testing a push to github.
+                Let's hope this works...
+                Neque, pariatur odio.
                 Maxime, labore nihil illo ea voluptate suscipit aliquid animi
                 possimus laudantium fugit obcaecati harum?
                 Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur

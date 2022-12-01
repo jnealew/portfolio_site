a4324b9a6f0e2d2077eccda04127cc017c00c313
Jack Neale <jack@192-168-1-113.tpgi.com.au>
2022-12-01 11:03:28 +1100

    001

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,9 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                My name is Jack Neale, I am testing a push to github.
-                Let's hope this works...
-                Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                Persistent, detail-oriented cyber security specialist with 2 years of experience in IT roles. Certificate holder of (ISC) 2 Certified in Cybersecurity℠. The successful completion of this certificate demonstrates the knowledge required for mid & entry level positions in the cybersecurity industry. Harvard University computer science (CS50) course near completion. Harvard University is renowned for its world-class education and has provided strong foundational skills in computer science that I have further developed.
+                Director of Netflix Documentary on music artist ‘Tones and I’.  
+                Division 1 student-athlete with 3.5 GPA. High-level athletic organisations do not normally see high academic achievement in their programs as the demands between athletics and intensive degrees prove to be very challenging. Taking on both as a student while achieving high results has demonstrated my motivation, discipline, and strong time management skills between travel, workouts, and academics. It has also demonstrated my ability to work individually to strengthen skills while working collectively with a team to meet the goal of winning.  
             </p>
         </section>
     );
